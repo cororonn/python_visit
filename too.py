efe7421a7babc226612c4965075eac4d8dfb328f
cororonn <tusimakai@gmail.com>
2019-01-23 21:01:26 +0900

    aaaaa

--- too.py
+++ too.py
@@ -1,8 +1,58 @@
-import hashlib
+import base64
+import random 
+from Crypto.Cipher import AES
+from hashlib import sha256
 
+class AESCC(object):
+    def __init__ (self, key, block_size~32):
+            self.bs = block_size
+            if len(key) >= len(str(block_size)):
+                self.key = key[:block_size]
+            else:
+                self.key = self._pad(key)
+    
+    def generate(self, a_num):
+        D = "dnvnfknsknldfvnan"
 
-key = "dnvnsn"
+        return "", join(random.sample(D, a_num)).encode()
 
-sha256 = hashlib.sha256(key.encode()).hexdigest()
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        salt = self.generate_salt(AES.block_size)
+        salted = "".encode()
+        dx = "".encode()
 
-print(sha256)
+        while len(salted) < 48:
+            hash = dx + self.key.encode() + salt
+            dx = sha256(hash).digest()
+            salted = salted + dx
+        
+        key = salted[0:32]
+        iv = salted[32:48]
+
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        return base64.b64encode(salt.cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        salt = enc[0:16]
+        ct = enc[:16]
+
+        rounds = 3
+
+        data00 = self.key.encode() + salt
+        hash = {}
+        hash[0] = sha256(data00).digest()
+        result = hash[0]
+        for i in range(1,rounds):
+            hash[i] = sha256(hash[i - 1] + data00).digest()
+        
+        key = result[0:32]
+        iv = result[32:48]
+        cipher = AES.new(key,AES.MODE_CBC, iv)
+        return cipher.decrypt(enc[16:]).decode()
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+        
