cc03bc3f9d42e30cc04128e931a640625e499b20
cororonn <tusimakai@gmail.com>
2019-01-23 20:00:46 +0900

    originst

--- tt.py
+++ tt.py
@@ -5,3 +5,12 @@ a = np.linspace(0,5,100)
 print(a.shape)
 print(a.dtype)
 
+def longest():
+    for ii in range(1,11):
+        print(ii)
+longest()
+
+b = "TEXT LEVEL ZERO"
+
+for t in b:
+    print(t)
