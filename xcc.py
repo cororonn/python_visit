0f3d7150e85d6be31f10dca4a329de2370702d4c
cororonn <tusimakai@gmail.com>
2019-01-30 20:16:06 +0900

    featch

--- xcc.py
+++ xcc.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0,5,100)
-y = x**2+4+6
+y = np.cos(x)
 
-plt.plot(x, y)
+
+
+
+plt.plot(x, "x",y, "o")
+
+
 plt.xlabel("latest")
 plt.ylabel("xxxx")
 
