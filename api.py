12042d1a51f5e9d414d150fe74dceda6a8a79728
cororonn <safellone@gmail.com>
2019-04-06 10:48:25 +0900

    aaaaaa

--- api.py
+++ api.py
@@ -1,5 +1,34 @@
+import requests
 import json
+import pprint
+import hashlib
+from Misskey import Misskey
+from websocket import create_connection
+
+api_Token="!wLno8dw52KYCvyn20VUQqRVJKYbiDRKi"
+app_secret = "NxM8T3Oyl6KhUImauM9ndQhp7klPixAR"
+
+h = "https://misskey.m544.net/api/auth/session/generate?i=NxM8T3Oyl6KhUImauM9ndQhp7klPixAR"
+
+item_data = {
+    "appsecret":"NxM8T3Oyl6KhUImauM9ndQhp7klPixAR",
+    "Content-Type": "/auth/session/generate"
+
+}
 
 
-api_token = "!wLno8dw52KYCvyn20VUQqRVJKYbiDRKi"
-secret = "NxM8T3Oyl6KhUImauM9ndQhp7klPixAR"
+r_post = requests.post(h,json=item_data)
+
+print(r_post.status_code)
+print(r_post)
+
+j = hashlib.sha256(api_Token.encode()).hexdigest()
+l = hashlib.sha256(app_secret.encode()).hexdigest()
+i = j+l
+
+misskey = Misskey("misskey.m544.net", apiToken="!wLno8dw52KYCvyn20VUQqRVJKYbiDRKi")
+
+h = "https://misskey.m544.net/api/auth/session/generate/?i=NxM8T3Oyl6KhUImauM9ndQhp7klPixAR"
+host_url = h.encode("unicode-escape")
+
+print(host_url)
